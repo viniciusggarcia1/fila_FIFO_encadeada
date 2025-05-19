492dcc89ede4a363a447f47dd369fa1cfaf08bc1
viniciusggarcia1 <viniciusggarcia1@hotmail.com>
2025-05-18 22:55:08 -0300

    integrating Rick and Morty API and render characters on frontend

--- script.js
+++ script.js
@@ -72,3 +72,48 @@ function printQueue(queue) {
     }
     console.log(elements);
 }
+
+// Now we will develop the Rick and Morty Queue, usind the Rick and Morty API
+
+const charsContainer = document.querySelector(".chars-container");
+const API = "https://rickandmortyapi.com/api";
+
+const defaultFilters = {
+    name: "",
+    species: "",
+    gender: "",
+    status: "",
+    page: 1
+};
+
+async function getCharacters({name, species, gender, status, page=1}) {
+    const response = await fetch(
+        `${API}/character/?name=${name}&species=${species}&gender=${gender}&status=${status}&page=${page}`
+    );
+
+    const characters = await response.json();
+
+    return characters.results;
+}
+
+async function render(characters) {
+    characters.forEach((character) => {
+        return charsContainer.innerHTML += `
+            <div class="char">
+                <img src="${character.image}" alt="${character.image}">
+                <div class="char-info">
+                <h3>${character.name}</h3>
+                <span>${character.species}</span>
+                </div>
+            </div>
+            `}
+    )
+}
+
+async function main () {
+    const characters = await getCharacters(defaultFilters);
+    render(characters);
+}
+
+main ();
+
